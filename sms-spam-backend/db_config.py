bc7dc1c3ed21f240e80b6803c374d73c2c450068
Aditya <150001244+AdityaKaashyap@users.noreply.github.com>
2025-09-15 21:45:14 +0530

    Update db_config.py

--- sms-spam-backend/db_config.py
+++ sms-spam-backend/db_config.py
@@ -1,7 +1,10 @@
 from pymongo import MongoClient
 
-# Replace with your MongoDB URI
-client = MongoClient("mongodb://localhost:27017/")  # or MongoDB Atlas URI
-db = client["spam_app"]
+MONGO_URI = "mongodb+srv://adityapk333_db_user:AObQAgBBMJJo6GmF@smish-collector.zaxj8tc.mongodb.net/?retryWrites=true&w=majority&appName=Smish-collector"
+client = MongoClient(MONGO_URI)
+db = client["cybertextshield"]
+
 users_collection = db["users"]
 permissions_collection = db["permissions"]
+ham_messages_collection = db["ham_messages"]
+smish_messages_collection = db["smish_messages"]
